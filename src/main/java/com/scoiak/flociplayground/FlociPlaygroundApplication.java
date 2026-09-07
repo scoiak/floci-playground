bd0f998270818eafeda72f712b724cb53acf0c41
scoiak <kaiocs@live.com>
2026-09-07 17:48:32 -0300

    Initial commit

---
+++ src/main/java/com/scoiak/flociplayground/FlociPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.scoiak.flociplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlociPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlociPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scoiak/flociplayground/FlociPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoiak.flociplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlociPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
